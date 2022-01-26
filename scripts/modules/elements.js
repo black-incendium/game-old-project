51f79ec1c0c3591758cbfe23d8dedaf746adb4f3
black-incendium <cryingincendium@gmail.com>
2022-01-26 23:38:58 +0100

    added canvas initialization

--- scripts/modules/elements.js
+++ scripts/modules/elements.js
@@ -1,5 +1,21 @@
-export let elements = {
-    initialize(){
-        console.log('works fine!')
+let elements = (()=>{
+    let canvas, ctx;
+    function initialize(){
+
+        if (canvas || ctx) return;
+
+        canvas = document.querySelector('canvas');
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        ctx = canvas.getContext('2d');
+        ctx.fillStyle = "red";
+        ctx.fillRect(100,100,500,500)
     }
-}
+
+    return {
+        initialize
+    }
+})();
+
+export {elements};
