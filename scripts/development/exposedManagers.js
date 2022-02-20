6bc034e8c40e471bcfa0b009c9558e3298d1e40e
black-incendium <cryingincendium@gmail.com>
2022-02-20 23:46:49 +0100

    create global reference to managers for development

---
+++ scripts/development/exposedManagers.js
@@ -0,0 +1,23 @@
+import { cameraManager } from './../managers/cameraManager.js';
+import { drawManager } from './../managers/drawManager.js';
+import { eventsManager } from './../managers/eventsManager.js';
+import { gameManager } from './../managers/gameManager.js';
+import { gameQueueManager } from './../managers/gameQueueManager.js';
+import { mapManager } from './../managers/mapManager.js';
+import { resizeManager } from './../managers/resizeManager.js';
+import { userInputManager } from './../managers/resizeManager.js';
+import { testManager } from './testManager.js';
+
+let exposedManagers = {
+    cameraManager,
+    drawManager,
+    eventsManager,
+    gameManager,
+    gameQueueManager,
+    mapManager,
+    resizeManager,
+    userInputManager,
+    testManager
+}
+
+export { exposedManagers }
