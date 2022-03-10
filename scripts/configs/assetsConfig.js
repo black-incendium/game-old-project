e6f6b8f949a093f30f55d4abda423b1248c2a4b0
black-incendium <cryingincendium@gmail.com>
2022-03-10 19:08:59 +0100

    add player_idle asset

---
+++ scripts/configs/assetsConfig.js
@@ -0,0 +1,11 @@
+let assets = {
+    images: [
+        {
+            name: 'player_idle_',
+            file: 'images/player_idle.png',
+            json: 'jsons/player_idle.json'
+        }
+    ]
+}
+
+export { assets };
