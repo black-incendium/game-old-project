c578c989ac22cbda0970d201a78bb1bc79f1192a
black-incendium <cryingincendium@gmail.com>
2022-03-17 22:40:57 +0100

    add images to assetsConfig

--- scripts/configs/assetsConfig.js
+++ scripts/configs/assetsConfig.js
@@ -1,4 +1,8 @@
 let assetsConfig = {
+    images: {
+        'playerIdle': 'player_idle.png',
+        'basicTilemap': 'basic_tilemap.png'
+    },
     imagesJsons: [
         'player_idle_assets.json',
         'basic_tileset.json'
